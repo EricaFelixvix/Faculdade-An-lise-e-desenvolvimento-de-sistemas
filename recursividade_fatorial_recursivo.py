d74ea37b883131d3d515aec7e03970a87b0b9b91
Erica Felix De Oliveira <129320356+EricaFelixvix@users.noreply.github.com>
2024-06-27 11:21:39 -0300

    codigos feitos em aula
    Recursividade é uma técnica de programação onde uma função chama a si mesma para resolver subproblemas menores do problema original. É frequentemente utilizada em problemas que podem ser divididos em partes menores e resolvidos de maneira semelhante, como em algoritmos de ordenação, pesquisa em árvores, e cálculo de sequências matemáticas.

---
+++ recursividade_fatorial_recursivo.py
@@ -0,0 +1,21 @@
+def fatorial_recursivo(n):
+    # Inicializa a variável fat com 1, que será usada para os casos base
+    fat = 1
+    # Verifica se o número é negativo, caso seja retorna None
+    if n < 0:
+        return None
+    # Se o número for 0 ou 1, o fatorial é 1
+    elif n == 0 or n == 1:
+        return fat
+    else:
+        # Retorna n multiplicado pelo fatorial de n - 1 (chamada recursiva)
+        return n * fatorial_recursivo(n - 1)
+
+# Testa a função com o valor 0, imprimindo o resultado
+print(fatorial_recursivo(0))
+
+
+'''Fatorial Recursivo:
+
+No método recursivo, a função chama a si mesma para 
+calcular o fatorial:'''
