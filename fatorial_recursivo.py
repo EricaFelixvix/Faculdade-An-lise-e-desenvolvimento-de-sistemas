c8b34401e69210c9df3698648cc392591421688b
Erica Felix <vixerica0726@gmail.com>
2024-07-02 18:11:56 -0300

    mais exemplos

---
+++ fatorial_recursivo.py
@@ -0,0 +1,10 @@
+# Fatorial recursivo 
+def fatorial(n):
+    fat = 1
+    if n == 0 or n == 1:
+        return fat 
+    else:
+        return n * fatorial(n - 1)
+# programa prncipal
+x = fatorial(5)
+print(x)
--- propriedade_multiplicacao.py
+++
@@ -1,6 +0,0 @@
-# Quando tem dependencia dos   conjuntos de dados multiplicamod
-
-def boo(dadosA, dadosB):
-    for i in dadosA:
-        for j in dadosB:
-            print(i + j)
